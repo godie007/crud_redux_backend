7b252f6afcce978deb30bcd906dffbde0753f709
godie007 <diegof.e3@gmail.com>
2017-08-23 11:22:49 -0500

    models updated

--- models.js
+++ models.js
@@ -3,6 +3,4 @@
 exports = module.exports = function (app, mongoose) {
   // embeddable docs first
   require('./schema/User')(app, mongoose)
-  require('./schema/Appointment')(app, mongoose)
-  require('./schema/AppointmentType')(app, mongoose)
 }
